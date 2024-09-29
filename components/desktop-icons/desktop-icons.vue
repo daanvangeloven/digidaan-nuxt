8d7b1878aa6042246a9fdd2823d304ca6f8d2b4c
Daan van Geloven <daanvangeloven@gmail.com>
2024-09-29 17:09:25 +0200

    added desktop icons

---
+++ components/desktop-icons/desktop-icons.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="desktop-icons">
+    <div
+      v-for="icon in data.items"
+      :key="icon.modal"
+      class="desktop-icon"
+      :style="{ top: icon.top, left: icon.left }"
+    >
+      <img :src="`/img/icons/${icon.icon}`" alt="icon" class="icon-img" />
+      <span>{{ icon.title }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import data from "@/assets/json/startmenu.json";
+
+export default {
+  data() {
+    return {
+      data,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/style/index";
+
+$img-size: 32px;
+$img-size-sm: 24px;
+
+.desktop-icons {
+  position: absolute;
+  top: 10px;
+  left: 5px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  font-size: 13px;
+  gap: 16px;
+  color: $text-light;
+}
+
+.desktop-icon {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+}
+
+.icon-img {
+  width: $img-size;
+  height: $img-size;
+  display: flex;
+}
+
+@media (max-width: 600px) {
+  .desktop-icons {
+    top: 5px;
+    left: 5px;
+    gap: 10px;
+    font-size: 11px;
+  }
+
+  .icon-img {
+    width: $img-size-sm;
+    height: $img-size-sm;
+  }
+}
+</style>
